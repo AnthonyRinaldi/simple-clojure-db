7a0785c7af849572c714077f0748bd2f2d3e4d36
cbolgiano <bolgianoc@teds.com>
2015-03-02 15:40:01 -0500

    Refactored code.

--- src/myApp/web.clj
+++ src/myApp/web.clj
@@ -5,7 +5,7 @@
 ;;            [myApp.db :as db]
             [cheshire.core :as json]
             [compojure.route :as route]
-            [compojure.core :refer (ANY GET POST defroutes)]
+            [compojure.core :refer (ANY GET POST PUT defroutes)]
             [ring.util.response :refer (response redirect content-type)]
             [clojure.pprint :refer (pprint)]
             [environ.core :refer (env)])
@@ -18,33 +18,37 @@
   (-> (response (with-out-str (pprint request)))
     (content-type "text/plain")))
 
+(def to-json json/generate-string)
+
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application.json"}
+   :body (to-json data)})
+
 ;;Create a post.
 (defn create [params]
   (let [id (post/create params)]
-    {:status 201
-     :headers {"Content-Type" "application/json"}
-     :body (json/generate-string (assoc params :_id (str id)))}))
+    (json-response  (assoc params :_id (str id)) 201)))
 
 ;;Fetch record from db.
 (defn fetch [id]
   (let [object-id (ObjectId. id)
         post (post/fetch object-id)]
-    {:status 200
-     :headers {"Content-Type" "application/json"}
-     :body (json/generate-string (assoc post :_id (str id)))}))
+    (json-response (assoc post :_id (str id)) 200)))
 
 ;;url endpoints...
 (defroutes routes
   (POST "/posts" {params :params} (create params))
   (GET "/posts/:id" [id] (fetch id))
+  (GET "/" []
+        (json-response "Welcome to myApp."))
   ;;Prints any request to body.
   (ANY "*" [] echo))
 
 (defn -main [& {:as args}]
-;; (db/init-db "mytestdb")  
+;; (db/init-db "dev")  
  (web/run
     (-> routes
-      (immutant/wrap-session {:timeout 20}))
-   
+      (immutant/wrap-session {:timeout 20}))   
     (merge {"host" (env :myApp-web-host), "port" (env :myApp-web-port)}
       args)))
